0f3a1083b1d0ed271e670cb22ab60f5fef9022fa
JiangHua <3504839020@qq.com>
2020-04-01 11:17:37 +0800

    Create test.java

---
+++ test.java
@@ -0,0 +1,18 @@
+package softwareTest;
+
+public class test {
+
+    public static void main(String[] args) {
+
+        int a = 0.5;
+
+        int b = "test";
+
+        int c = 1;
+        String d = "D";
+        c = d;
+
+        int e = 1;
+        e.length();
+    }
+}
